2a8252714a10d43016860564e0b231deb4884003
abrarafi <rafi.ahrijm@gmail.com>
2025-11-12 01:55:25 +0600

    updated

--- frontend/lib/api.ts
+++ frontend/lib/api.ts
@@ -3,7 +3,7 @@ import { refreshToken } from "./auth";
 import router from "next/router";
 
 const api = axios.create({
-  baseURL: process.env.NEXT_PUBLIC_API_URL || "http://localhost:5000",
+  baseURL: process.env.NEXT_PUBLIC_API_URL || "https://soulcare-jsyf.onrender.com",
   headers: { "Content-Type": "application/json" },
   withCredentials: true,
 });
